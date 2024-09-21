9083a7e34c3897a059551771df44216585ba56cf
alulacoder <rasalula2024@gmail.com>
2024-09-21 09:05:03 +0100

    Add files via upload

--- satellite.py
+++ satellite.py
@@ -81,7 +81,7 @@ next_satellite = 0
 
 start_time = 0
 total_time = 0
-end_time = 0
+game_over = False
 
 number_of_satellite = 8
 
@@ -92,7 +92,6 @@ def create_satellites():
         satellite.pos = randint(40, WIDTH-40), randint(40, HEIGHT-40)
         satellites.append(satellite)
     start_time = time()
-
 
 def draw():
     global total_time
@@ -107,27 +106,35 @@ def draw():
     for line in lines:
         screen.draw.line(line[0], line[1], (255,255,255))
 
-    if next_satellite < number_of_satellite:
+    if not game_over:
         total_time = time() - start_time
         screen.draw.text(str(round(total_time,1)), (10,10), fontsize=30)
     else:
-        screen.draw.text(str(round(total_time,1)), (10,10), fontsize=30)
-
+        if next_satellite == number_of_satellite:
+            screen.draw.text("You Win!", color="green", midtop=(WIDTH/2, HEIGHT/2 - 50), fontsize=60)
+        else:
+            screen.draw.text("You Lose!", color="red", midtop=(WIDTH/2, HEIGHT/2 - 50), fontsize=60)
 
 def update():
-    pass
+    global game_over, total_time
+
+    if not game_over:
+        total_time = time() - start_time
+        if total_time >= 30:  # Changed to 30 seconds
+            game_over = True
 
 def on_mouse_down(pos):
-    global next_satellite, lines
+    global next_satellite, lines, game_over
 
-    if next_satellite < number_of_satellite:
-        if satellites[next_satellite].collidepoint(pos):
-            if next_satellite:
-                lines.append((satellites[next_satellite-1].pos, satellites[next_satellite].pos))
-            next_satellite = next_satellite + 1
-        else:
-            lines = []
-            next_satellite = 0
+    if not game_over:
+        if next_satellite < number_of_satellite:
+            if satellites[next_satellite].collidepoint(pos):
+                if next_satellite:
+                    lines.append((satellites[next_satellite-1].pos, satellites[next_satellite].pos))
+                next_satellite = next_satellite + 1
+            else:
+                lines = []
+                next_satellite = 0
 
 create_satellites()
 
